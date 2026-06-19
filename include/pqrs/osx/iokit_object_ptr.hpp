9dba1f1132bb9c445084087875e10171e7ba4999
Takayama Fumihiko <tekezo@pqrs.org>
2026-06-19 21:03:43 +0900

    v3.2.0

--- include/pqrs/osx/iokit_object_ptr.hpp
+++ include/pqrs/osx/iokit_object_ptr.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-// pqrs::osx::iokit_object_ptr v3.1
+// pqrs::osx::iokit_object_ptr v3.2.0
 
 // (C) Copyright Takayama Fumihiko 2018.
 // Distributed under the Boost Software License, Version 1.0.
